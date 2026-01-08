2e25bdc3b9f6b53990ffae339405f2964652624b
s991116 <philip.juhl@gmail.com>
2026-01-08 14:36:13 +0100

    Lagt get email authentication i log med stacktrace

--- function_app.py
+++ function_app.py
@@ -11,25 +11,34 @@ from src import inreach_functions as inreach_func
 
 app = func.FunctionApp()
 
-# auth_service = email_func.gmail_authenticate()
+_auth_service = None
+
+def get_auth_service():
+    global _auth_service
+    if _auth_service is None:
+        try:
+            logging.info("Authenticating Gmail")
+            _auth_service = email_func.gmail_authenticate()
+        except Exception:
+            logging.exception("gmail_authenticate() failed")
+            raise
+    return _auth_service
 
 @app.function_name(name="mytimer")
-@app.timer_trigger(schedule="0 */2 * * * *", 
-              arg_name="mytimer",
-              run_on_startup=False) 
+@app.timer_trigger(schedule="0 */2 * * * *", arg_name="mytimer")
 def test_function(mytimer: func.TimerRequest) -> None:
     logging.info("HELLO - Timer triggered")
 
     if mytimer.past_due:
         logging.info("The timer is past due")
 
-#    result = email_func.process_new_inreach_message(auth_service)
-#
-#    if result:
-#        grib_path, garmin_reply_url = result
-#        encoded_grib = saildoc_func.encode_saildocs_grib_file(grib_path)
-#        inreach_func.send_messages_to_inreach(garmin_reply_url, encoded_grib)
-#
-#        logging.info("GRIB sent")
-#    else:
-#        logging.info("No messages")
+    auth = get_auth_service()
+    # result = email_func.process_new_inreach_message(auth)
+
+    # if result:
+    #     grib_path, garmin_reply_url = result
+    #     encoded_grib = saildoc_func.encode_saildocs_grib_file(grib_path)
+    #     inreach_func.send_messages_to_inreach(garmin_reply_url, encoded_grib)
+    #     logging.info("GRIB sent")
+    # else:
+    #     logging.info("No messages")
